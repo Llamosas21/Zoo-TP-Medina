e1df6839cf136c9d2c501856bbbb1c2913601812
santiago <santiago@debian.com>
2024-11-05 17:36:04 -0300

    Eliminé el archivo tpIntegrador.rar

--- obj/Debug/net8.0/tpIntegrador.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpIntegrador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpIntegrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e793c99ae76bcd551d2b7451179540067b27b74c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140be39829566fb77f857c4bd7e1077276bbf61e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpIntegrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpIntegrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
